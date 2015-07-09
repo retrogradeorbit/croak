2e7f4cdb6540fc34197de37381252cc78e22446d
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-09 22:42:25 +0800

    prober mainline

---
+++ src/croak/prober.clj
@@ -0,0 +1,38 @@
+(ns croak.prober
+  (:require [croak.core :as core]
+            [croak.time :refer [wait-until next-aligned-time]]
+            [clj-time.core :as time]
+
+            [croak.probes.iptables :refer [iptables]]))
+
+(defn prober
+  "prober main loop"
+  [config]
+  (let [step (-> config :delay)
+        align-times (-> config :align-times)
+        next-tick (if align-times
+                    (next-aligned-time step)
+                    (time/now))]
+    (when align-times (wait-until next-tick))
+
+    (loop [t next-tick]
+      (let [n (->> step
+                   time/millis
+                   (time/plus t))]
+
+        (swap! core/=data= assoc t
+               (let [data (iptables)]
+                 {:INPUT (-> data :INPUT :bytes)
+                  :OUTPUT (-> data :OUTPUT :bytes)}))
+
+        (wait-until n)
+        (recur n)))))
+
+
+(comment
+
+  (def f (future (prober)))
+
+  (future-cancel f)
+
+)
