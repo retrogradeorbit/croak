307e9f71329ca0bea57743a5b3ddc9fa697389c3
Crispin Wellington <crispinw@signiq.com>
2015-07-09 18:29:57 +0800

    first commit

---
+++ src/croak/core.clj
@@ -0,0 +1,7 @@
+(ns croak.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/croak/core_test.clj
@@ -0,0 +1,7 @@
+(ns croak.core-test
+  (:require [clojure.test :refer :all]
+            [croak.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
