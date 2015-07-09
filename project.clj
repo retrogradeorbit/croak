c7c9871a44bec21332345be299ef4f282bfb91e6
Crispin Wellington <crispinw@signiq.com>
2015-07-09 18:33:06 +0800

    updated project with license and description

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject croak "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A remote host health monitoring and reporting application"
+  :url "https://github.com/retrogradeorbit/croak"
+  :license {:name "GNU General Public License version 3"
+            :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :main ^:skip-aot croak.core
   :target-path "target/%s"
