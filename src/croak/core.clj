d22cf6ac28f39d3fb47d6e1b1772ae62096fb3f8
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-10 22:11:08 +0800

    command line

--- src/croak/core.clj
+++ src/croak/core.clj
@@ -1,10 +1,15 @@
 (ns croak.core
+  (:require [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
 ;; the ram storage of the probers results
 (def =data= (atom {}))
 
+(def cli-options
+  [["-c" "--config CONFIG" "Configuration file"
+    :default nil]
+   ["-h" "--help"]])
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (parse-opts args cli-options)))
