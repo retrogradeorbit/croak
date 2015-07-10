8fbb865c1d56821044d4b84b3239ac1638d4352a
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-10 22:10:44 +0800

    archiver and start of file storage

--- src/croak/archiver.clj
+++ src/croak/archiver.clj
@@ -1,15 +1,62 @@
-(ns croak.archiver)
+(ns croak.archiver
+  (:require [clj-time.format :as format]
+            [me.raynes.fs :as fs]
+            [clojure.java.io :as io]))
 
-(comment
-  (defn archive-watcher
-  "watcher for data func that regularly writes it
-  to file when size exceeds trigger"
-  [key refr old n]
-  (when (>= (count n) archive-count)
-    (let [timestamps (->> n keys sort (take archive-count))
-          to-write (into {} (for [t timestamps] [t (n t)]))]
-      (future (println "writing timestamps!" archive-count)
-              (spit (make-filename (first timestamps)) (prn-str to-write))
-              (swap! refr
-                     (fn [data]
-                       (apply dissoc data timestamps))))))))
+
+(def ^:dynamic *image-storage-path* "/tmp/storage")
+
+(def time-formatter (format/formatter "yyyy-MM-dd HH:mm:ss.SSS"))
+
+
+(defn init-storage!
+  ([]
+   (init-storage! *image-storage-path*))
+  ([path]
+   (when-not (fs/exists? path)
+     (when-not (fs/mkdir path)
+       (throw (ex-info (str "Can't create storage path:" path)
+                       {:type ::fs-exception}))))))
+
+
+(defn make-filename [timestamp]
+  (->> (str "data-" (format/parse time-formatter timestamp) ".dat")
+       (io/file *image-storage-path*)))
+
+
+(defn get-filenames []
+  (let [items
+        (filter seq
+                (map
+                 #(->> %
+                       str
+                       (re-seq #"data-(\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+Z).dat")
+                       first
+                       reverse)
+                 (fs/list-dir *image-storage-path*)))
+
+        fname-parse (fn [[ind fname]]
+                      [(format/parse time-formatter ind) fname])]
+    (into
+     {}
+     (map fname-parse items))))
+
+
+(defn archive-watcher
+  "builds a watcher function that behaves according to the values in
+  the hashmap `config`"
+  [{:keys [archive-count]}]
+  ^{:doc "returns a future that runs a watcher for data func that regularly
+  writes it to file when size exceeds trigger. if the write succeeds
+  remove those entries from the atom."}
+  (fn
+    [key refr old n]
+    (when (>= (count n) archive-count)
+      (let [timestamps (->> n keys sort (take archive-count))
+            to-write (into {} (for [t timestamps] [t (n t)]))]
+        (future
+          (spit (make-filename (first timestamps)) (prn-str to-write))
+          (swap! refr
+                 (fn [data]
+                   ;; remove all the writen timestamps from atom
+                   (apply dissoc data timestamps))))))))
