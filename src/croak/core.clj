2ca779813be4f26c7ac98ac147d48659d6fa7d3f
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-11 16:22:34 +0800

    clean shutdown

--- src/croak/core.clj
+++ src/croak/core.clj
@@ -9,23 +9,32 @@
     :default nil]
    ["-h" "--help"]])
 
-(defn -main
-  [& args]
-  (println (parse-opts args cli-options)))
-
 (defn testit []
-  (def f (future (prober/prober
-                  {:delay 5000
-                   :align-times true
-                   :debug true}
-                  )))
-
   (add-watch prober/=data= :archiver (archiver/archive-watcher {:archive-count 5
                                                                 :debug true}))
 
-  (comment
-    (future-cancel f))
+  ;(iptables)
+  ;(assert false "WAT")
+  (def fff (future (prober/prober
+                    {:delay 5000
+                     :align-times true
+                     :debug true}
+                    )))
 
-)
+  fff
 
-(testit)
+
+
+  #_ (comment
+    (future-cancel f)
+    )
+  )
+
+
+(defn -main
+  [& args]
+  (println (parse-opts args cli-options))
+  (try (deref (testit))
+       (finally
+         ;; http://dev.clojure.org/jira/browse/CLJ-959
+         (shutdown-agents))))
