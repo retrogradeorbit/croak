0b368123f26fd4bd0fc9399a165d916631606674
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-11 13:20:58 +0800

    test confirms edn

--- test/croak/archiver_test.clj
+++ test/croak/archiver_test.clj
@@ -10,4 +10,4 @@
       (is
        (=
         (str (make-filename (time/date-time 1975 11 20 6 30 30 0)))
-        "/storage/data-1975-11-20-06:30:30.000.dat")))))
+        "/storage/data-1975-11-20-06:30:30.000.edn")))))
