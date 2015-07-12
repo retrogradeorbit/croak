a994b0c61ff11c6b4e30aaf2649b5fd85d541f37
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-12 11:45:37 +0800

    add http-kit

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [clj-time "0.8.0"]
                  [org.clojure/tools.cli "0.3.1"]
                  [me.raynes/fs "1.4.6"]
+                 [http-kit "2.1.18"]
                  ]
   :main ^:skip-aot croak.core
   :target-path "target/%s"
