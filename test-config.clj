a2d2be5791f21f69ba6afd3d92f5254ca1ed6692
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-18 19:30:49 +0800

    sample config file to development

---
+++ test-config.clj
@@ -0,0 +1,37 @@
+{
+ :probes
+ [
+  {:probe :iptables
+   :delay 1000
+   :align-times true
+   :debug true}
+
+  {:probe :meminfo
+   :delay 20000
+   :align-times true
+   :debug true}
+  ]
+
+ :archiver
+ {
+  ;; put the files here
+  :storage "/tmp/storage"
+
+  ;; how many records to store per file
+  :count 1000
+
+  ;; print what you're doing
+  :debug true
+  }
+
+ :reporter
+ {
+  ;; http-kit options
+  :opts
+  {
+   :method :post
+   :url "http://localhost5000/data"
+   }
+
+  }
+}
