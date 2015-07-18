9c5f8f4a0469e7847e8f2bd799702578e17fb092
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-18 09:11:25 +0800

    upload all the files sequentially

--- src/croak/reporter.clj
+++ src/croak/reporter.clj
@@ -1,7 +1,8 @@
 (ns croak.reporter
   (:require [org.httpkit.client :as http]
             [croak.storage :as storage]
-            [clojure.data.json :as json])
+            [clojure.data.json :as json]
+            [clojure.java.io :as io])
 )
 
 (def ^:dynamic *base-opts*
@@ -20,18 +21,23 @@
         @(http/request d)]
     (if error
       (println "Failed, exception: " error)
-      (println "HTTP success: " status))))
+      (println "HTTP success: " status))
+    status))
 
 (defn reporter
   "reporter mainline function"
   [config]
-  (loop []
+  (loop [remaining (sort (storage/get-filenames))]
     ;(wait-until-up "localhost.localdomain")
 
     ;; upload any files first
-    (let [[time filename] (first (sort (storage/get-filenames)))]
+    (let [[time filename] (first remaining)]
       (println "loading" filename)
-      (-> filename
-           storage/load-edn
-           (send-data {:url "http://localhost:5000/data"
-                       :method :post})))))
+      (when (= 200 (-> filename
+                       storage/load-edn
+                       (send-data {:url "http://localhost:5000/data"
+                                   :method :post})))
+        (println "deleting" filename)
+        (io/delete-file (str "/tmp/storage/" filename)))
+      (when (seq (rest remaining))
+        (recur (rest remaining))))))
