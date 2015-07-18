cf275f81020cead9059b1e1047093b7c92e30ae7
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-18 19:30:25 +0800

    the new meminfo probe

---
+++ src/croak/probes/meminfo.clj
@@ -0,0 +1,29 @@
+(ns croak.probes.meminfo
+  (:require [clojure.java.shell :as shell]
+            [clojure.java.io :as io]
+            [clojure.string :as string]))
+
+(defn camel-to-hyphen [s]
+  (->> s
+       (re-seq #"[A-Z][a-z]*")
+       (map string/lower-case )
+       (string/join "-")))
+
+(defn meminfo-line [[k v]]
+  [(keyword k) (read-string v)])
+
+(defn meminfo []
+  (into {} (map
+            #(->> %
+                  (re-seq #"(\S+):\s+(\d+)")
+                  first
+                  next
+                  meminfo-line
+                  )
+
+            (string/split-lines
+             ;; https://clojuredocs.org/clojure.core/slurp
+             ;; On Linux, some JVMs have a bug where they cannot read a file in the /proc
+             ;; filesystem as a buffered stream or reader.  A workaround to this JVM issue
+             ;; is to open such a file as unbuffered:
+             (slurp (java.io.FileReader. "/proc/meminfo"))))))
