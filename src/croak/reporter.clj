bf904cea7cc6cfc16902f4b669c96cd6c9a0b68d
Crispin Wellington <retrogradeorbit@gmail.com>
2015-07-18 14:35:25 +0800

    upload from memory now too!

--- src/croak/reporter.clj
+++ src/croak/reporter.clj
@@ -1,6 +1,7 @@
 (ns croak.reporter
   (:require [org.httpkit.client :as http]
             [croak.storage :as storage]
+            [croak.prober :as prober]
             [clojure.data.json :as json]
             [clojure.java.io :as io])
 )
@@ -24,20 +25,60 @@
       (println "HTTP success: " status))
     status))
 
+(defn upload-file [filename]
+  (println "loading" filename)
+  (when (= 200 (-> filename
+                   storage/load-edn
+                   (send-data {:url "http://localhost:5000/data"
+                               :method :post})))
+    (println "deleting" filename)
+    (io/delete-file (str "/tmp/storage/" filename)))
+  )
+
+(defn upload-data [data]
+  (println "uploading ram" (count data))
+  (when (= 200 (-> data
+                   storage/data->disk
+                   (send-data {:url "http://localhost:5000/data"
+                               :method :post})))
+    (println "cleaning ram of" (count data))
+    (swap! prober/=data=
+           (fn [d]
+             (apply dissoc d (keys data))))))
+
 (defn reporter
   "reporter mainline function"
-  [config]
+  [{:keys [minimum-set minimum-send]
+    :or {:minimum-set 3
+         :minimum-send 10
+         }
+    :as config}]
+  (println "reporter :" (sort (storage/get-filenames)))
+
+    ;; upload all files, oldest to newest
+
   (loop [remaining (sort (storage/get-filenames))]
-    ;(wait-until-up "localhost.localdomain")
+                                        ;(wait-until-up "localhost.localdomain")
 
     ;; upload any files first
-    (let [[time filename] (first remaining)]
-      (println "loading" filename)
-      (when (= 200 (-> filename
-                       storage/load-edn
-                       (send-data {:url "http://localhost:5000/data"
-                                   :method :post})))
-        (println "deleting" filename)
-        (io/delete-file (str "/tmp/storage/" filename)))
-      (when (seq (rest remaining))
-        (recur (rest remaining))))))
+    (if-let [[time filename] (first remaining)]
+      (upload-file filename))
+
+    (when (seq (rest remaining))
+      (recur (rest remaining))))
+
+    ;; if no more files, try uploading memory
+
+  (let [data @prober/=data=
+        num (count data)]
+    (println "num" num)
+    (when (>= num (+ minimum-send minimum-set))
+      ;; upload all leave minimum-set behind
+      (let [report-count (- num (minimum-set))
+            timestamps (->> data keys sort (take report-count))
+            to-report (into {} (for [t timestamps] [t (data t)]))]
+        (println "rc:" report-count)
+        (upload-data to-report))))
+
+  (Thread/sleep 10000)
+  (recur config))
