890eeab7bb513c0b796d7c4083f2fe0a907cfbd1
Crispin Wellington <retrogradeorbit@gmail.com>
2015-09-24 08:59:58 +0800

    core async dep

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "GNU General Public License version 3"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [clj-time "0.8.0"]
                  [org.clojure/tools.cli "0.3.1"]
                  [me.raynes/fs "1.4.6"]
