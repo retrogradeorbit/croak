1e08bca93096cf3899caf08052f5299e03ca19dd
Crispin Wellington <retrogradeorbit@gmail.com>
2015-09-26 15:22:39 +0800

    pass in hostname and opts

--- src/croak/reporter.clj
+++ src/croak/reporter.clj
@@ -11,86 +11,73 @@
    :timeout 4000
    :user-agent "Croak/0.1"})
 
-(defn send-data [data opts]
+(defn send-data [data opts hostname]
   (let [d (assoc (into *base-opts* opts)
                  :form-params
                  {
-                  :host "knives"
+                  :host hostname
                   :data (json/write-str data)}
                  )
         {:keys [status headers body error] :as resp}
         @(http/request d)]
     (when error
-      (println "Failed, exception: " error)
-      ;(println "HTTP success: " status)
-      )
+      (println "Reporter send-data failed: " (str error)))
     status))
 
-(defn upload-file [filename]
-  ;(println "loading" filename)
+(defn upload-file [filename opts hostname]
   (if (= 200 (-> filename
-                   storage/load-edn
-                   ;println
-                   (send-data {:url "http://localhost:5000/data"
-                               :method :post})))
-    (do ;(println "deleting" filename)
-        (io/delete-file (str "/tmp/storage/" filename))
-        true)
+                 storage/load-edn
+                 (send-data opts hostname)))
+    (do
+      (io/delete-file (str "/tmp/storage/" filename))
+      true)
     false))
 
-(defn upload-data [data]
-  ;(println "uploading ram" (count data))
+(defn upload-data [data opts hostname]
   (when (= 200 (-> data
                    storage/data->disk
-                   (send-data {:url "http://localhost:5000/data"
-                               :method :post})))
-    ;(println "cleaning ram of" (count data))
+                   (send-data opts hostname)))
     (swap! prober/=data=
            (fn [d]
              (apply dissoc d (keys data))))))
 
 (defn reporter
   "reporter mainline function"
-  [{:keys [minimum-set minimum-send]
+  [{:keys [minimum-set minimum-send
+           fail-delay success-delay
+           hostname opts]
     :or {minimum-set 3
          minimum-send 10
+         fail-delay 7000
+         success-delay 10000
+         hostname (.getHostName (InetAddress/getLocalHost))
+         opts {:method :post}
          }
     :as config}]
-  ;; (println "reporter :" (sort (storage/get-filenames)))
-  ;; (println "minimum-set" minimum-set)
-  ;; (println "minimum-send" minimum-send)
-
-    ;; upload all files, oldest to newest
-
+  ;; upload all files, oldest to newest
   (loop [remaining (sort (storage/get-filenames))]
-                                        ;(wait-until-up "localhost.localdomain")
+    ;(wait-until-up "localhost.localdomain")
 
-    ;(println "." (first remaining))
     ;; upload any files first
     (if-let [time-file (first remaining)]
-      (if (upload-file (second time-file))
+      (if (upload-file (second time-file) opts hostname)
         ;; upload success
         (when (seq (rest remaining))
           (recur (rest remaining)))
 
         ;; upload again
-        (do (Thread/sleep 7000)
+        (do (Thread/sleep fail-delay)
             (recur remaining)))))
 
-    ;; if no more files, try uploading memory
-
+  ;; if no more files, try uploading memory
   (let [data @prober/=data=
         num (count data)]
-    ;(println "num" num)
     (when (>= num (+ minimum-send minimum-set))
       ;; upload all leave minimum-set behind
-      ;(println "upload")
       (let [report-count (- num minimum-set)
             timestamps (->> data keys sort (take report-count))
-            ;_ (println "!" timestamps)
             to-report (into {} (for [t timestamps] [t (data t)]))]
-        ;(println "rc:" report-count)
-        (upload-data to-report))))
+        (upload-data to-report opts hostname))))
 
-  (Thread/sleep 10000)
+  (Thread/sleep success-delay)
   (recur config))
